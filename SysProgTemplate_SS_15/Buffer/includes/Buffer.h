547fd6138ead23ec8bac3f378e7a6b2c6ac9464b
keih93 <nnquang7114@gmail.com>
2016-03-23 21:15:22 +0100

    Buffer1.0
    versuchen getchar() zu bauen.

---
+++ SysProgTemplate_SS_15/Buffer/includes/Buffer.h
@@ -0,0 +1,23 @@
+/*
+ * Buffer.h
+ *
+ *  Created on: Sep 26, 2012
+ *      Author: knad0001
+ */
+
+#ifndef BUFFER_H_
+#define BUFFER_H_
+
+class Buffer {
+	char* buffer2;
+	char* buffer1;
+	int fd;
+	int stand;
+public:
+	Buffer(const char *pathname);
+	virtual ~Buffer();
+	char* getchar();
+	char* ungetchar();
+	};
+
+#endif /* BUFFER_H_ */
---
+++ SysProgTemplate_SS_15/Buffer/src/Buffer.cpp
@@ -0,0 +1,46 @@
+/*
+ * Buffer.cpp
+ *
+ *  Created on: Sep 26, 2012
+ *      Author: knad0001
+ */
+
+#include "../includes/Buffer.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+
+Buffer::Buffer(const char *pathname) {
+	
+	buffer1 = new char[512];
+	buffer2 = new char[512];
+	fd = open(pathname, O_DIRECT);
+		
+}
+
+Buffer::~Buffer() {
+	delete[] buffer;
+}
+
+char* Buffer::getchar() {
+	stand = read(fd, buffer1, 512);
+	int i = 0;
+	while(i<stand){
+	return buffer1[i];
+	i++;
+	}
+	i = 0;
+	if (stand > 512) {
+	stand = read(fd,buffer2, 512);
+	}
+	
+	while(i<stand){
+		return buffer2[i];
+		i++;
+	}
+	
+	
+}
+
