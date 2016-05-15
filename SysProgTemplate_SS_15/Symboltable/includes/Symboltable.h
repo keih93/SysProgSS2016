14066825076b4110e17c55be458e37d3d7e85ea9
keih93 <nnquang7114@gmail.com>
2016-05-15 20:22:52 +0200

    Symboltabelle header files

--- SysProgTemplate_SS_15/Symboltable/includes/Symboltable.h
+++ SysProgTemplate_SS_15/Symboltable/includes/Symboltable.h
@@ -4,14 +4,21 @@
  *  Created on: Sep 26, 2012
  *      Author: knad0001
  */
-
+#include "Information.h"
+#include "StringTab.h"
+#include "SymtabEntry.h"
 #ifndef SYMBOLTABLE_H_
 #define SYMBOLTABLE_H_
 
 class Symboltable {
 public:
-	Symboltable();
-	virtual ~Symboltable();
+	char* insert(char* lexem);
+
+private:
+	SymtabEntry* sym;
+	StringTab* table;
+	Information* lookup(char* key);
+
 
 };
 
